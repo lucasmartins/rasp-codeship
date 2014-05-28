5794d8a735a00565b37b759240a62a5e681d2fe2
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-05-28 18:53:37 -0300

    refactored to prettier code

--- start.rb
+++ start.rb
@@ -3,55 +3,50 @@ require 'json'
 class CodeshipBuildStatus
   TOKEN = ENV['CODESHIP_TOKEN']
   PROJECT = ENV['CODESHIP_PROJECT']
+  PIN_MAP = {running: 18, broken: 17}
 
   def self.run!
     raise 'I need both CODESHIP_TOKEN and CODESHIP_PROJECT env var to be set!' unless ENV['CODESHIP_TOKEN'] && ENV['CODESHIP_PROJECT']
     boot_pins
     trap("SIGINT") do
       puts 'Bye bye!'
-      pin(17,:off) # alert the script is down!
+      pin(:broken,:off) # alert the script is down!
       exit!
     end
     puts 'Starting...'
     while true do
       if build_status==:success
         puts 'Build is good'
-        pin(17,:off)
-        pin(18,:off)
+        pin(:broken,:off)
+        pin(:running,:off)
       else
         if build_status==:testing
           puts 'Building ...'
-          pin(17,:off)
-          pin(18,:off)
+          pin(:broken,:off)
+          pin(:running,:off)
           sleep 0.1
-          pin(18,:on)
+          pin(:running,:on)
         else
           puts 'Build is bad'
-          pin(17,:on)
-          pin(18,:off)
+          pin(:broken,:on)
+          pin(:running,:off)
         end
       end
     end
   end
 
   def self.boot_pins
-    `echo '17' > /sys/class/gpio/unexport`
-    `echo '17' > /sys/class/gpio/export`
-    `echo out > /sys/class/gpio/gpio17/direction`
-    `echo '18' > /sys/class/gpio/unexport`
-    `echo '18' > /sys/class/gpio/export`
-    `echo out > /sys/class/gpio/gpio17/direction`
-    pin(17,:on)
-    pin(18,:on)
-    sleep 0.1
-    pin(17,:off)
-    pin(18,:off)
-    sleep 0.1
-    pin(17,:on)
-    pin(18,:on)
-    sleep 0.1
-    pin(17,:off)
-    pin(18,:off)
+    initialize_pin(17, :out)
+    initialize_pin(18, :out)
+    blink([:broken,:running], 0.1, :on, :off)
+    blink([:broken,:running], 0.1, :on, :off)
+  end
+
+  def self.initialize_pin(number, direction)
+    raise "Unkown direction #{status}" unless [:in,:out].include?(direction)
+    `echo '#{number}' > /sys/class/gpio/unexport`
+    `echo '#{number}' > /sys/class/gpio/export`
+    `echo #{direction.to_s} > /sys/class/gpio/gpio#{number}/direction`
   end
 
   def self.build_status
@@ -62,13 +57,24 @@ class CodeshipBuildStatus
     return status.to_sym
   end
 
-  def self.pin(number,status)
+  def self.pin(name,status)
+    raise "Unkown pin #{name}" unless PIN_MAP.include?(name)
+    raise "Unkown status #{status}" unless [:on,:off].include?(status)
+    number = PIN_MAP[name]
     if status==:on
       `echo 1 > /sys/class/gpio/gpio#{number}/value`
     elsif status==:off
       `echo 0 > /sys/class/gpio/gpio#{number}/value`
-    else
-      raise 'wft?'
+    end
+  end
+
+  def self.blink(pin_names,time_gap=0.1, start_state=:on, end_state=:off)
+    pin_names.each do |name|
+      pin(name,start_state)
+    end
+    sleep time_gap
+    pin_names.each do |name|
+      pin(name,end_state)
     end
   end
 end
